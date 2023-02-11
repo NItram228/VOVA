f998f79d7536fda2ef48a2f4c36ad00678f47a6b
NItram228 <martin.kluka.m@gmail.com>
2023-02-11 14:55:54 +0100

    vova

---
+++ lesson_1/js/main.js
@@ -0,0 +1,10 @@
+var person = null;
+if(confirm("Ты уверен?")){
+    person = prompt("Введи имя");
+        alert(person + " Тебя заскамили");
+    for(var i = 0; i = 100; i + 10){
+        console.log(i);
+    }
+}else{
+    alert("Нахуй ты сюда нажал? Ты должен был нажать <да> ")
+}
